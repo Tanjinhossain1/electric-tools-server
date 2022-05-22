ac957787f7a3534604f048702fe9fd33df7ceab0
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-22 13:36:37 +0600

    add mongodb or load tools

--- index.js
+++ index.js
@@ -12,14 +12,25 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://electricTools:<password>@cluster0.f9lm3.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f9lm3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect()
-        const ToolsCollection = client.db("Products").collection("tools");
+        const toolsCollection = client.db("Products").collection("tools");
+
+        app.get('/tools',async(req,res)=>{
+            const tools = await toolsCollection.find().toArray();
+            res.send(tools)
+        })
+        app.get('/tools/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const findTool = await toolsCollection.findOne(query);
+            res.send(findTool)
+        })
 
     }
     finally {
